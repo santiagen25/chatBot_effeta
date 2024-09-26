8835c7b17ee1a1b07c7b8c3bdd5b63ae0d381602
Santiago Torrabadella <santiago.torrabadella@gmail.com>
2024-09-26 12:59:15 +0200

    minor improvements

--- index.js
+++ index.js
@@ -398,17 +398,17 @@ client.on('message_create', async (msg) => {
                     "!records: cuenta los emoticonos de cada persona, y crea una tabla de records con las personas que han introducido más emoticonos.\n\n"+
                     "!help, !effeta, !ayuda: Ayuda general.\n\n"+
                     "!comandos: Se muestran estos comandos.\n\n"+
-                    "!info: Información del ChatBot-Effeta.\n\n"+
+                    "!info u !hola: Información del ChatBot-Effeta.\n\n"+
                     "!cuantoqueda: dias que quedan para el retiro.\n\n"+
                     "!punteros: De donde se sacan los datos, y desde cuando.\n\n"+
                     "!calcula cat: Torna el càlcul en Català")
     } else if (msg.body === "!info" || msg.body === "!hola") {
         msg.reply("¡Hola! Soy ChatBot-Effeta, y estoy programado para contar de forma automática los emoticonos del grupo de oración.\n"+
-                    "Por favor, para facilitarme el trabajo sigue las siguientes reglas:\n\n"+
+                    "Por favor, para facilitarme el trabajo sigue las siguientes indicaciones:\n\n"+
                     "Siempre que quieras poner la cantidad de un emoticono utiliza los siguientes formatos:\n\t🙏3\n\t🙏x3\n\t🙏X3\n\t🙏*3\n\t🙏🙏🙏\n\n"+
                     "Puedes escribir espacios entre emoticonos, símbolos y números, o puedes escribirlo todo junto, no me importa:\n\t🙏 x 3\n\t🙏x3\n\t🙏x 3\n\t🙏 x3\n\n"+
                     "Recuerda siempre poner los emoticonos a la derecha, pues los emoticonos a la izquierda no los puedo leer bien...\n\t_Si me escribes:_\n\t🙏\n\t5🌹\n\t_Creere que has realizado 5 oraciones en lugar de 5 misterios del rosario._\n\n"+
-                    "No escribas decimales, pues tampoco los entiendo bien...\n\t_Si me escribes:_\n\t🙏3,5\n\t_Creere que has realizado solo 3 oraciones en lugar de 3,5.\n\n"+
+                    "No escribas decimales, pues tampoco los entiendo bien...\n\t_Si me escribes:_\n\t🙏3,5\n\t_Creere que has realizado solo 3 oraciones en lugar de 3,5._\n\n"+
                     "Si te has equivocado en algún mensaje, puedes borrarlo o editarlo. Yo solo leeré todos los mensajes cuando alguien escriba !calcula\n\n"+
                     "Si quieres escribir algún mensaje y te preocupa poner emoticonos que yo pueda contar, ten en cuenta que yo solo contaré los mensajes en los que encuentre emoticonos en los primeros 7 carácteres, por ejemplo:\n\t*¡Hola! ¿Que tal estás? Yo como una 🌹*\n\t_Este mensaje no lo contaré_\n\t*La 🌹 de mi casa es muy bonita*\n\t_Este mensaje sí lo contaré_\n\n"+
                     "Creo que ya sabes todo lo que necesitas.\n\n"+
@@ -447,7 +447,7 @@ client.on('message_create', async (msg) => {
                         "\nMensajes leidos: *"+mensajesLeidos+"*\n"+
                         "Horas totales: *"+horasTotales+"*";
         msg.reply(horasRespuesta)
-    } 
+    }
 });
 
 function calculameEsteEmoji (currentmsg, emoji, nombreEmoji, variable, varMoment) {
